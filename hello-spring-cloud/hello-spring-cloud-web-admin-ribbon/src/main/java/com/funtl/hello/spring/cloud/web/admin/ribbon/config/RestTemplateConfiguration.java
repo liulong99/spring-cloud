cb59865b0eb1bd2d16696e36fcb3dc9fb3a3374d
liulong <706750966@qq.com>
2019-10-15 23:34:25 +0800

    hello-spring-cloud
    Signed-off-by: liulong <706750966@qq.com>

---
+++ hello-spring-cloud/hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/config/RestTemplateConfiguration.java
@@ -0,0 +1,7 @@
+package com.funtl.hello.spring.cloud.web.admin.ribbon.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration//使服务负载均衡
+public class RestTemplateConfiguration {
+}
